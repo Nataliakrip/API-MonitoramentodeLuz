d646b70bb217717a3183015a59669ad990a38bdb
Natalia Silva <nataliapsilva36@gmail.com>
2024-07-23 14:09:14 -0300

    feat: adicionando arquivo database

---
+++ src/database/database.js
@@ -0,0 +1,9 @@
+//Dados ficticios, os dados reais deverão ser monitorados através
+// dos nossos equipamentos de hardware LDR que vai ser monitorado 
+// por um Arduino ligado a um microcontrolador que converte os sinais elétricos (analogicos)
+// em sinais digitais
+export const lights = [
+    { id: 1, value: 650, timestamp: '2024-07-21T12:00:00Z' },
+    { id: 2, value: 1010, timestamp: '2024-07-21T13:00:00Z' },
+    { id: 3, value: 350, timestamp: '2024-07-22T14:00:00Z' }
+  ];
